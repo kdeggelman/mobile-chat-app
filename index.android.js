8ecd65f8dc4d4299b2234d4277f2c2a97cb26d35
kdeggelman <kdeggelman@gmail.com>
2015-10-28 16:41:42 -0700

    Bare bones app that shows messages in a list

--- index.android.js
+++ index.android.js
@@ -1,7 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
@@ -9,44 +5,119 @@ var {
   AppRegistry,
   StyleSheet,
   Text,
+  TextInput,
   View,
+  ListView
 } = React;
 
-var MobileMessage = React.createClass({
+var ChatBox = React.createClass({
+  getInitialState: function() {
+    var ds = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2,
+    });
+    return {
+      messageList: [],
+      dataSource: ds
+    };
+  },
+  componentDidMount: function() {
+  },
+  addMessage: function(message) {
+    console.log("Adding a new message " + message);
+    var messages = this.state.messageList;
+    var newMessages = messages.concat([message]);
+    this.setState({
+      messageList: newMessages,
+      dataSource: this.state.dataSource.cloneWithRows(newMessages)
+    });
+  },
+  handleMessageSubmit: function(message) {
+    console.log("Submitting message " + message);
+    this.addMessage(message);
+  },
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+      <View>
+        <Text style={styles.title}>
+          {'Messages'}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
+        <MessageList data={this.state.dataSource}/>
+        <InputForm onMessageSubmit={this.handleMessageSubmit} />
+      </View>
+    );
+  }
+});
+
+var MessageList = React.createClass({
+  render: function() {
+    return (
+      <ListView
+        dataSource={this.props.data}
+        renderRow={this.renderMessage}
+        style={styles.listView}
+      />
+    );
+  },
+  renderMessage: function(message) {
+    return (
+      <View style={styles.message}>
+        <Text>
+          {message.author}: {message.text}
         </Text>
       </View>
     );
   }
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+var InputForm = React.createClass({
+  getInitialState: function() {
+    return {
+      input: ''
+    };
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  handleSubmit: function() {
+    var text = this.state.input;
+    console.log('Submitted message ' + text);
+    if (!text) {
+      return;
+    }
+    this.props.onMessageSubmit({author: "author", text: text});
+    this.setState({input: ''});
+    return;
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  render: function() {
+    return (
+      <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        autoFocus={true}
+        value={this.state.input}
+        onChangeText={(text) => this.setState({'input': text})}
+        onSubmitEditing={this.handleSubmit}
+        placeholder={'Message'}
+      />
+    );
+  }
 });
 
-AppRegistry.registerComponent('MobileMessage', () => MobileMessage);
+var styles = StyleSheet.create({
+  listView: {
+    marginTop: 20,
+    backgroundColor: '#5E93FF',
+  },
+  message: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#5E93FF',
+    fontSize: 300,
+    paddingTop: 10,
+  },
+  title: {
+    fontSize: 50,
+    margin: 10,
+    textAlign: 'center',
+  }
+});
+
+AppRegistry.registerComponent('MobileMessage', () => ChatBox);
